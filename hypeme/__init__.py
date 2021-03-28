44f3983c9fc2610a04cd15263d7be5c4a4777f49
sneakers-the-rat <JLSaunders987@gmail.com>
2021-03-27 18:20:34 -0700

    colors and also handling escape codes

--- hypeme/__init__.py
+++ hypeme/__init__.py
@@ -1,23 +1,72 @@
 import builtins
-from random import randint
+import re
+from random import randint, choice
 
 _QUIETER = False
 _LAST_PRINT = None
 
-def _hype(printme):
+ESCAPE_CHARS = ('\n', '\r', '\t', '\b', '\f')
+
+
+class Colors:
+    # background colors with white text
+    COMBOS = ['\033[1;4{col_ind}m'.format(col_ind=i) for i in range(1,7)]
+    BOLD = '\033[1m'
+    RESET = '\033[0m'
+
+def _make_it_yell(in_string:str) -> str:
+    """
+    CAPITALIZE EVERYTHING EXCEPT ESCAPE SEQUENCES AND THEN MAKE IT COLORY
+    """
+
+    # strip any other ansi color codes present in the string
+    in_string = re.sub(r'\x1b\[[0-9;]+m', '', in_string)
+
+    # lil more cleanup
+    in_string = in_string.lstrip("'").rstrip("'")
+
+    # pick a color to start the out_string
+    out_str = choice(Colors.COMBOS) + Colors.BOLD
+
+    # iterate through string to capitalize!
+    # this is super slow probs but waht do you want from a freaking novelty package
+    for char in in_string:
+        if char in ESCAPE_CHARS:
+            out_str += char
+            continue
+        else:
+            out_str += char.upper()
+
+    # add some amount of exclaims
+    out_str += "!"*randint(1, 10)
+
+    # append color reset
+    out_str += Colors.RESET
+
+    return out_str
+
+
+def _hype(printme, *args, **kwargs):
     """
     NO DOCS!!!!!! JUST HYPE!!!!!!!!!!!!
     """
     global _QUIETER
     global _LAST_PRINT
 
+    # if just printing a newline, like ipython does for some reason,
+    # just do it
+    if printme in ESCAPE_CHARS:
+        builtins.ye_olde_print(printme, *args, **kwargs)
+        return
+
     _LAST_PRINT = printme
 
     try:
-        builtins.ye_olde_print(str(repr(printme)).upper().lstrip("'").rstrip("'") + "!"*randint(1, 10))
+        its_yelly_now = _make_it_yell(str(repr(printme)))
+        builtins.ye_olde_print(its_yelly_now, *args, **kwargs)
     except:
         if _QUIETER:
-            builtins.ye_olde_print(printme)
+            builtins.ye_olde_print(printme, *args, **kwargs)
         else:
             builtins.ye_olde_print("IF I CANT YELL IT THEN I WONT PRINT IT"+"!"*randint(10,100))
 
